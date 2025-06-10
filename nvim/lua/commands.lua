5aca9ae200e04396cfd348dcc875b81891a8ab35
ellingsvee <elling.svee@gmail.com>
2025-06-10 11:20:03 +0200

    Move nvim to separate directory

--- nvim/lua/commands.lua
+++
@@ -1,5 +0,0 @@
-vim.api.nvim_create_user_command('ReloadSnippets', function()
-  package.loaded['snippets.my_snippets'] = nil -- replace with your actual module
-  require('luasnip.loaders.from_lua').lazy_load { paths = './lua/luasnip/' }
-  print 'Snippets reloaded!'
-end, {})
